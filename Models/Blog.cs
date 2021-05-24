32a934ab265760bbe91035c5a3f8cbd9dd421626
Melia88 <castro.melissa8.com>
2021-05-24 09:18:53 -0600

    s

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -8,13 +8,13 @@ namespace bloggerServer.Models
     public string CreatorId { get; set; }
     public Account Creator { get; set; }
     [Required]
-    [MaxLength(20)]
+    [MaxLength(24)]
 
     public string Title { get; set; }
 
     public string Body { get; set; } = "No Description";
     public string ImgUrl { get; set; } = "http://placehold.it/200x200";
-    public bool Published { get; set; } = false;
+    public bool Published { get; set; }
   }
 }
 
